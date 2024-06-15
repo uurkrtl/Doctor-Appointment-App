76e66d5efca67b2fe5e3a68e629f5319efaff314
Ugur Kartal <kartalugur@gmail.com>
2024-06-15 12:45:30 +0200

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
