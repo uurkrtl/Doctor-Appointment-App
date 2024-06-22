fa7a54a4a40df7b551a4f743c986af375d14fab4
Ugur Kartal <kartalugur@gmail.com>
2024-06-22 13:33:08 +0200

    refactor code

--- frontend/src/pages/AppointmentDetailAdd.tsx
+++ frontend/src/pages/AppointmentDetailAdd.tsx
@@ -4,10 +4,10 @@ import AppointmentService from "../services/AppointmentService.ts";
 
 function AppointmentDetailAdd() {
     const { id = '' } = useParams<string>();
-    const navigate = useNavigate();
-    const appointmentService = new AppointmentService();
     const [imageUrl, setImageUrl] = useState<string>('');
     const [description, setDescription] = useState<string>('');
+    const navigate = useNavigate();
+    const appointmentService = new AppointmentService();
     const [errorMessage, setErrorMessage] = useState<string>('');
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -15,8 +15,7 @@ function AppointmentDetailAdd() {
         appointmentService.updateAppointmentScore(id, imageUrl, description)
             .then(response => {
                 console.log(response)
-                //navigate(`/appointments/add-date/${response.data.id}`)
-                navigate('/appointments/add-date/' + response.data.id)
+                navigate(`/appointments/add-date/${response.data.id}`)
             })
             .catch(error => {
                 if (error.response) {
@@ -38,7 +37,7 @@ function AppointmentDetailAdd() {
                         d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 3.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5H2.545c-.3 0-.545.224-.545.5m6.5 5a.5.5 0 0 0-1 0V10H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V11H10a.5.5 0 0 0 0-1H8.5z"/>
                 </svg>
                 <h2>Neuer Termin</h2>
-            </div>ID:{id}ABC
+            </div>
 
             <div className="row g-5">
                 <div className="col-md-12 col-lg-12">
